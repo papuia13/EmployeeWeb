f8b358a944d146f473dcd11b96aca9660042613b
Lalramnunmawia <80774787+papuia13@users.noreply.github.com>
2025-08-14 02:32:13 +0530

    Initiallizing Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './pages/header/Header';
+import Dashboard from './pages/dashboard/dashboard';
+import Crud from './pages/dashboard/crud';
+import NoMatch from './pages/noMatch/noMatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path='/employee' element={<Crud />} />
+        <Route path='*' element={<NoMatch />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/dashboard/crud.js
@@ -0,0 +1,6 @@
+const Crud = () => {
+    return (
+        <><h1>Welcome to the CRUD Page</h1></>
+    )
+}
+export default Crud;
